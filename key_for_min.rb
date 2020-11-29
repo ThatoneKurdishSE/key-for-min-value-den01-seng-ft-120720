3b4226c6ea272958c57b74a059bf27a73b9b4e6c
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-29 02:11:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,8 @@ def key_for_min_value(name_hash)
   min_value= nil
   name_hash.each do |key, value|
     if min_value==nil || value<min_value
-      min_key=key
-      min_value=value
-end
+  min_key=key
+  min_value=value
 end
 end
 min_key
